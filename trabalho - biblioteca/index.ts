b39eb9c82b7710d38f59d08dd6c7bef3ca36da36
Caua Rodrigues <rodrigzs.soare@gmail.com>
2024-03-21 12:06:52 -0300

    atualizando

--- trabalho - biblioteca/index.ts
+++ trabalho - biblioteca/index.ts
@@ -1,13 +1,4 @@
-import { Genero } from "./GenerosLivro";
 import { LivroInfo } from "./LivroInfo";
+import { Genero } from "./GenerosLivro";
 import { StatusLivros } from "./StatusLivro";
-import { Status } from "./classStatus";
-
-let LivroD: Status   = new Status(StatusLivros.Disponivel, Genero.FANTASIA)
-let LivroE: LivroInfo = new LivroInfo(StatusLivros.Emprestado, Genero.FICCAO )
-let LivroA: LivroInfo = new LivroInfo(StatusLivros.Atrasado, Genero.ROMANCE)
-
-
-
-
 
